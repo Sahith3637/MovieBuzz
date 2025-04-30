1398e7140c6bad0e044640a26911acaa9bf1a543
Sai Sahith <sajju@vivejaitservices.com>
2025-04-30 16:43:59 +0530

    bookingsUpdate

--- MovieBuzz.API/obj/Debug/net8.0/MovieBuzz.API.AssemblyInfo.cs
+++ MovieBuzz.API/obj/Debug/net8.0/MovieBuzz.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieBuzz.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b68635572847f861aff03a21faf1963e12754db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95442377ea3164b8626a316f0a3b6c1349a01abf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieBuzz.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieBuzz.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MovieBuzz.Services/obj/Release/net8.0/MovieBuzz.Services.AssemblyInfo.cs
+++ MovieBuzz.Services/obj/Release/net8.0/MovieBuzz.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieBuzz.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dba75de424eacdc1331f6c417b7267f24aecb85b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95442377ea3164b8626a316f0a3b6c1349a01abf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieBuzz.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieBuzz.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
